d40b96a49773c84a3075e7bf3925e427cc88ac12
Rajatku301999mar <58697453+Rajatku301999mar@users.noreply.github.com>
2020-10-09 11:00:38 +0530

    Create Weather_Observation_Station_9.sql

---
+++ SQL/Basic Select/Weather_Observation_Station_9.sql
@@ -0,0 +1,5 @@
+select distinct city from station
+where city Not like 'A%' and city Not like 'E%' and city Not like 'I%'
+and  city Not like 'O%' and city not like 'U%' and
+     city Not like 'a%' and city Not like 'e%' and city Not like 'i%' and
+      city Not like 'o%' and city not like 'u%';
