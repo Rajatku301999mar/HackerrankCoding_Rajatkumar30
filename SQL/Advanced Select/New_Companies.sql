6290b78419be8dcd5131d9c92c4a2beecfabbde9
Rajatku301999mar <58697453+Rajatku301999mar@users.noreply.github.com>
2020-10-16 13:33:48 +0530

    Create New_Companies.sql

---
+++ SQL/Advanced Select/New_Companies.sql
@@ -0,0 +1,5 @@
+select C.company_code, C.founder, COUNT(DISTINCT E.lead_manager_code), COUNT(DISTINCT E.senior_manager_code), COUNT(DISTINCT E.manager_code), COUNT(DISTINCT E.employee_code) from Company C
+INNER JOIN Employee E
+ON C.company_code = E.company_code
+GROUP BY C.company_code, C.founder
+ORDER BY C.company_code ASC;
