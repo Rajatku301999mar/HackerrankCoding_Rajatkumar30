b1098c43ea441fdd2d1616278b47cf4203557696
Rajatku301999mar <58697453+Rajatku301999mar@users.noreply.github.com>
2021-01-12 15:28:22 +0530

    Create Set_.intersection()_Operation.py

---
+++ Python/Sets/Set_.intersection()_Operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+set_a=set(input().split())
+b=int(input())
+set_b=set(input().split())
+out=len(set_a.intersection(set_b))
+print(out)
